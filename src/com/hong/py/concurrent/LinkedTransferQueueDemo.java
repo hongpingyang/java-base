ea68bc8fb5c007766a8c04b630eb401d9a418044
Hongpy <hpymn123@163.com>
2020-04-16 17:43:44 +0800

    0000

--- src/com/hong/py/concurrent/LinkedTransferQueueDemo.java
+++ src/com/hong/py/concurrent/LinkedTransferQueueDemo.java
@@ -1,5 +1,6 @@
 package com.hong.py.concurrent;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedTransferQueue;
 import java.util.concurrent.TimeUnit;
 
@@ -9,7 +10,7 @@ import java.util.concurrent.TimeUnit;
  * description:
  * life for code
  *
- * 单向链表结构的无界阻塞队列
+ * 单向链表结构的无界非阻塞队列
  *
  *  -- 1 预占模式--
  * LinkedTransferQueue 采用一种预占模式。意思就是消费者线程取元素时，如果队列为空，那就生成一个节点（节点元素为null）入队，
@@ -25,12 +26,16 @@ import java.util.concurrent.TimeUnit;
  * -- 3节点自链接--
  * 已匹配节点的 next 引用会指向自身。
  *
+ *
+ * 非阻塞的理解：就是可以多个线程同时添加，多个线程同时获取。
  */
 public class LinkedTransferQueueDemo {
 
     public static void main(String[] args){
 
         LinkedTransferQueue<String> queue = new LinkedTransferQueue<>();
+        //ConcurrentLinkedQueue queue1 = new ConcurrentLinkedQueue();
+        //queue1.tr();
 
         Thread product=new Thread(()->{
             String take = null;
@@ -39,6 +44,7 @@ public class LinkedTransferQueueDemo {
                 System.out.println(take);
                /* take = queue.take(); //一直阻塞
                 System.out.println(take);*/
+
                 take = queue.poll(2, TimeUnit.SECONDS); //一直阻塞到2秒
                 System.out.println(take);
             } catch (InterruptedException e) {
@@ -48,6 +54,18 @@ public class LinkedTransferQueueDemo {
         });
         product.start();
 
+        Thread product1=new Thread(()->{
+            String take = null;
+            try {
+                take = queue.take(); //会阻塞在这里直到。queue.transfer("第二次");
+                System.out.println(take);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
+        product1.start();
+
         Thread consumer=new Thread(()->{
             try {
                 Thread.sleep(2000);
@@ -55,6 +73,22 @@ public class LinkedTransferQueueDemo {
                 e.printStackTrace();
             }
             queue.put("第一次");
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                //若当前存在一个正在等待获取的消费者线程，即立刻将e移交之；
+                // 否则将元素e插入到队列尾部，
+                // 并且当前线程进入阻塞状态，直到有消费者线程取走该元素
+                queue.transfer("第二次");
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("生产者1结束");
         });
         consumer.start();
 
